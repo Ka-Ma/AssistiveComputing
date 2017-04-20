11b7fd2f6b9b8244d05a85a1ce53e02d5d7c4d76
Katherine Mann <kkjayne@gmail.com>
2017-04-20 09:44:44 +0800

    initial

---
+++ AssistiveComputing/main.cpp
@@ -0,0 +1,16 @@
+//stub
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char rah;
+
+	cout << "This program will run the LED Matrix" << endl;
+	
+	//to make the console not disappear at conclusion of debug put a breakpoint here.
+	
+	return 0;
+}
