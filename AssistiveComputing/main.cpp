6d3b69f592fdfeedf0b39300c1ec36ef1809b10f
Katherine Mann <kkjayne@gmail.com>
2017-04-20 10:06:58 +0800

    using git submodule to add matrix library, added to linker in VS

--- AssistiveComputing/main.cpp
+++ AssistiveComputing/main.cpp
@@ -1,13 +1,17 @@
 //stub
 
 #include <iostream>
+#include "led-matrix.h"
+
+// Types exported by the RGB-Matrix library.
+using rgb_matrix::Canvas;
+using rgb_matrix::GPIO;
+using rgb_matrix::RGBMatrix;
 
 using namespace std;
 
 int main()
 {
-	char rah;
-
 	cout << "This program will run the LED Matrix" << endl;
 	
 	//to make the console not disappear at conclusion of debug put a breakpoint here.
