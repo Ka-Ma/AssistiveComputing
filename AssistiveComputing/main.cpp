362e474ba61ced353343c0807d90ffc0d2062334
Katherine Mann <kkjayne@gmail.com>
2017-05-18 15:12:03 +0800

    trying to get pi to load image, having issues.

--- AssistiveComputing/main.cpp
+++ AssistiveComputing/main.cpp
@@ -2,6 +2,20 @@
 
 #include <iostream>
 #include "led-matrix.h"
+#include "threaded-canvas-manipulator.h"
+#include "transformer.h"
+#include "graphics.h"
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#include <algorithm>
 
 // Types exported by the RGB-Matrix library.
 using rgb_matrix::Canvas;
@@ -9,12 +23,36 @@ using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 
 using namespace std;
+using namespace rbb_matrix;
+
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+	interrupt_received = true;
+}
 
 int main()
 {
+	//setup LED Matrix
+
+	//setup Buttons
+
+	//display screen
+	//read button
+	//act on button read depending on current screen
+
 	cout << "This program will run the LED Matrix" << endl;
 	
 	//to make the console not disappear at conclusion of debug put a breakpoint here.
+
+	
+	RGBMatrix::Options matrix_options;
+	rgb_matrix::RuntimeOptions runtime_opt;
+	RGBMatrix *matrix = CreateMatrixFromOptions(matrix_options, runtime_opt);
+	Canvas *canvas = matrix;
+		
+	
+
+	
 	
 	return 0;
 }
--- AssistiveComputing/test.cpp
+++ AssistiveComputing/test.cpp
@@ -1,9 +1,121 @@
 //to test how to draw my stuff to matrix
 
 #include <stdio.h>
+#include <signal.h>
 #include "../matrix/include/led-matrix.h" //needed to include relative dir otherwise it wouldn't compile
+#include "threaded-canvas-manipulator.h"
+#include <iostream>
+#include <unistd.h> // for f_ok
+#include <assert.h> // for assert
+#include <errno.h> // for errors
+#include <string.h>
 
 using rgb_matrix::RGBMatrix;
+using namespace std;
+
+#define TERM_ERR "\033[1;31m"
+#define TERM_NORM "\033[0m"
+
+
+struct Pixel {
+	Pixel() : red(0), green(0), blue(0){}
+	uint8_t red;
+	uint8_t green;
+	uint8_t blue;
+};
+
+struct Image {
+	Image() : width(-1), height(-1), image(NULL) {}
+	~Image() { Delete(); }
+	void Delete() { delete[] image; Reset(); }
+	void Reset() { image = NULL; width = -1; height = -1; }
+	inline bool IsValid() { return image && height > 0 && width > 0; }
+	const Pixel &getPixel(int x, int y) {
+		static Pixel black;
+		if (x < 0 || x >= width || y < 0 || y >= height) return black;
+		return image[x + width * y];
+	}
+
+	int width;
+	int height;
+	Pixel *image;
+};
+
+
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo){
+	interrupt_received = true;
+}
+
+// Read line, skip comments.
+char *ReadLine(FILE *f, char *buffer, size_t len) {
+	char *result;
+	do {
+		result = fgets(buffer, len, f);
+	} while (result != NULL && result[0] == '#');
+	return result;
+}
+
+Image LoadPPM(const char *filename) {
+	cout << "loading image"<<endl;
+	Image new_image_;
+	
+	FILE *f = fopen(filename, "r");
+	// check if file exists
+	if (f == NULL && access(filename, F_OK) == -1) {  //pi recognises the relevant headers included above
+		cout << "it can't open the file" << endl;
+		//error reports
+		perror("Error opening file");
+		printf("Error code opening file: %d\n", errno);
+		printf("Error opening file: %s\n", strerror(errno));
+		fprintf(stderr, "File \"%s\" doesn't exist\n", filename);
+		//return false;
+	}
+	//if (f == NULL) return 0;
+	cout << "starting to read file" << endl;
+	char header_buf[256];
+	const char *line = ReadLine(f, header_buf, sizeof(header_buf));
+#define EXIT_WITH_MSG(m) { fprintf(stderr, "%s: %s |%s", filename, m, line); fclose(f);}// return false; }
+	
+	if (sscanf(line, "P6 ") == EOF){
+		cout << "wrong file type" << endl;
+		EXIT_WITH_MSG("Can only handle P6 as PPM type.");
+	}
+	line = ReadLine(f, header_buf, sizeof(header_buf));
+	int new_width, new_height;
+	if (!line || sscanf(line, "%d %d ", &new_width, &new_height) != 2){
+		cout << "can't find dimensions" << endl;
+		EXIT_WITH_MSG("Width/height expected");
+	}
+	int value;
+	line = ReadLine(f, header_buf, sizeof(header_buf));
+	if (!line || sscanf(line, "%d ", &value) != 1 || value != 255){
+		cout << "over 255" << endl;
+		EXIT_WITH_MSG("Only 255 for maxval allowed.");
+	}
+	const size_t pixel_count = new_width * new_height;
+	cout << "got pixel count" << endl;
+	Pixel *new_image = new Pixel[pixel_count];
+	assert(sizeof(Pixel) == 3);   // we make that assumption.
+	if (fread(new_image, sizeof(Pixel), pixel_count, f) != pixel_count) {
+		line = "";
+		EXIT_WITH_MSG("Not enough pixels read.");
+	}
+//#undef EXIT_WITH_MSG
+	fclose(f);
+//	fprintf(stderr, "Read image '%s' with %dx%d\n", filename,
+//		new_width, new_height);
+	
+	//MutexLock l(&mutex_new_image_);
+	new_image_.Delete();  // in case we reload faster than is picked up
+	new_image_.image = new_image;
+	new_image_.width = new_width;
+	new_image_.height = new_height;
+	return new_image_;
+}
+
+
+
 
 int main(int argc, char **argv) {
 	// Set some defaults
@@ -27,16 +139,44 @@ int main(int argc, char **argv) {
 	// matrix->ApplyStaticTransformer(...);  // Optional
 
 	// Do your own command line handling with the remaining options.
+	//testing opening file
+	//FILE * test;
+	//test = fopen("I-AM-HERE", "w");
+	//fputs("testing test", test);
+	//if (test == NULL){ 
+	//	cout << "YOU SUCK" << endl;
+
+	//	//error reports
+	//	perror("Error opening file");
+	//	printf("Error code opening file: %d\n", errno);
+	//	printf("Error opening file: %s\n", strerror(errno));
+	//	exit(-1);
+	//	
+	//	fclose(test); 
+	//}
+	
+	cout<<"going to load image"<<endl;
+
+	//read in ppm to canvas
+	Image loaded = LoadPPM("MenuBlack.ppm");
+	cout<<"Theoretically an image was loaded\n"<<endl;
+	cout<<"image width = "<< loaded.width<<" image height = "<<loaded.height<<endl;
+
 
 	//  .. now use matrix
 	// 32x64, see if this makes interestng colours on LED matrix
-	for(int count = 0; count < 3000; count++){
-		for (int i = 0; i < 64; i++){
-			for (int j = 0; j < 32; j++){
-				matrix->SetPixel(i, j, i + j, i, j);
+	while(!interrupt_received){
+		//send image to screen
+		for (int i = 0; i < loaded.width; i++){
+			printf("line %d\n", i);
+			for (int j = 0; j < loaded.height; j++){
+				matrix->SetPixel(i, j,loaded.getPixel(i,j).red, loaded.getPixel(i,j).green, loaded.getPixel(i,j).blue);
 			}
 		}
 	}
+
+	signal(SIGTERM, InterruptHandler);
+	signal(SIGINT, InterruptHandler);
 
 	delete matrix;   // Make sure to delete it in the end.
 }
